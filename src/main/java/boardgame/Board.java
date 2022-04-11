ea940169ce1a28337a23e27bf40d9758ebe7eee7
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-04-11 19:00:23 -0300

    BoardException and defensive programming

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -7,37 +7,57 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column!");
+        }
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
     }
 
     public Piece piece(int rows, int columns) {
+        if (!positionExists(rows, columns)) {
+            throw new BoardException("Position not on the board!");
+        }
         return pieces[rows][columns];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board!");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
-    public void placePiece(Piece piece, Position position){
-        this.pieces[position.getRow()][position.getColumn()] = piece;
-        piece.position = position;
+    public void placePiece(Piece piece, Position position) {
+        if(thereIsAPieces(position)){
+            throw new BoardException("There is already a piece on position " + position);
+        }
+        this.pieces[position.getRow()][position.getColumn()]=piece;
+    piece.position =position;
+}
+
+    private boolean positionExists(int row, int column) {
+        return (row >= 0 && row < rows && column >= 0 && column < columns);
+    }
+
+    public boolean positionExists(Position positions) {
+        return positionExists(positions.getRow(), positions.getColumn());
+    }
+
+    public boolean thereIsAPieces(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board!");
+        }
+        return this.piece(position) != null;
     }
 
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
 }
