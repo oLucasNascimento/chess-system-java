9925e591cae16cdd9b933bb30827ae183f26a707
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-04-15 16:20:22 -0300

    Handling exceptions and clearing screen

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,32 +2,47 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     
     public static void main(String[] args) {
-    
+        
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         
         while (true) {
-            UserInterface.printBoard(chessMatch.getPieces());
-            System.out.println();
-
-            System.out.print("Source: ");
-            ChessPosition source = UserInterface.readChessPosition(sc);
-            System.out.println();
-    
-            System.out.print("Target: ");
-            ChessPosition target = UserInterface.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
-
+            try {
+                UserInterface.clearScreen();
+                UserInterface.printBoard(chessMatch.getPieces());
+                System.out.println();
+                
+                System.out.print("Source: ");
+                ChessPosition source = UserInterface.readChessPosition(sc);
+                System.out.println();
+                
+                System.out.print("Target: ");
+                ChessPosition target = UserInterface.readChessPosition(sc);
+                
+                ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+                System.out.print("Type enter to try again: ");
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+                System.out.print("Type enter to try again: ");
+                sc.nextLine();
+            }
         }
     }
     
